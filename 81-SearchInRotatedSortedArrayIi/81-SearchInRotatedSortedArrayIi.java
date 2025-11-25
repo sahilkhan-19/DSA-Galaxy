bd157082a9764c47692cedba4ff34d039a3df797
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2025-11-25 09:32:19 +0530

    Create 81-SearchInRotatedSortedArrayIi

---
+++ 81-SearchInRotatedSortedArrayIi/81-SearchInRotatedSortedArrayIi.java
@@ -0,0 +1,37 @@
+// Last updated: 11/25/2025, 9:32:18 AM
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int p = 0;
+        int q = n-1;
+        while(p<=q){
+            int mid = (p+q)/2;
+            if(nums[mid]==target) return true;
+            if(nums[p] == nums[mid] && nums[mid] == nums[q]){
+                p+=1;
+                q-=1;
+                continue;
+            }
+
+            //Left Sorted
+            if(nums[mid] >= nums[p]){
+                if(target >= nums[p] && target <= nums[mid]){
+                    q = mid -1;
+                }
+                else{
+                    p = mid + 1;
+                }
+            }
+            //Right Sorted
+            else{
+                if(target >= nums[mid] && target <= nums[q]){
+                    p = mid + 1;
+                }
+                else{
+                    q = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
