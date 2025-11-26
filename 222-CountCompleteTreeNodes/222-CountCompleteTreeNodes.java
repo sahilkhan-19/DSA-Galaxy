3a5d874dccc2976fabf5952ad444ed8c5e656f6d
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2025-11-26 20:10:29 +0530

    File updated at 11/26/2025, 8:10:27 PM

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -1,25 +1,25 @@
-// Last updated: 11/25/2025, 12:21:47 PM
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int countNodes(TreeNode root) {
-        if(root==null) return 0;
-        int lh = countNodes(root.left);
-        int rh = countNodes(root.right);
-
-        return lh+rh+1;
-    }
-}
+// Last updated: 11/26/2025, 8:10:27 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int countNodes(TreeNode root) {
+18        if(root==null) return 0;
+19        int lh = countNodes(root.left);
+20        int rh = countNodes(root.right);
+21
+22        return lh+rh+1;
+23    }
+24}
