eac5f5a58cedc7db2e15fd3d2dcfdd0e235027ae
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2025-12-02 20:22:12 +0530

    File updated at 12/2/2025, 8:22:09 PM

--- 493-ReversePairs/493-ReversePairs.java
+++ 493-ReversePairs/493-ReversePairs.java
@@ -1,35 +1,36 @@
-// Last updated: 12/1/2025, 9:14:44 PM
-class Solution {
-public int reversePairs(int[] nums) {
-if (nums == null || nums.length == 0) return 0;
-return mergeSort(nums, 0, nums.length - 1);
-}
-
-private int mergeSort(int[] nums, int left, int right) {
-    if (left >= right) return 0;
-    int mid = left + (right - left) / 2;
-    int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
-
-    // Count reverse pairs
-    int j = mid + 1;
-    for (int i = left; i <= mid; i++) {
-        while (j <= right && nums[i] > 2L * nums[j]) j++;
-        count += j - (mid + 1);
-    }
-
-    // Merge step
-    int[] temp = new int[right - left + 1];
-    int i = left, k = 0;
-    j = mid + 1;
-    while (i <= mid && j <= right) {
-        if (nums[i] <= nums[j]) temp[k++] = nums[i++];
-        else temp[k++] = nums[j++];
-    }
-    while (i <= mid) temp[k++] = nums[i++];
-    while (j <= right) temp[k++] = nums[j++];
-    System.arraycopy(temp, 0, nums, left, temp.length);
-
-    return count;
-}
-
-}
+// Last updated: 12/2/2025, 8:22:09 PM
+1class Solution {
+2public int reversePairs(int[] nums) {
+3if (nums == null || nums.length == 0) return 0;
+4return mergeSort(nums, 0, nums.length - 1);
+5}
+6
+7private int mergeSort(int[] nums, int left, int right) {
+8    if (left >= right) return 0;
+9    int mid = left + (right - left) / 2;
+10    int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+11
+12    // Count reverse pairs
+13    int j = mid + 1;
+14    for (int i = left; i <= mid; i++) {
+15        while (j <= right && nums[i] > 2L * nums[j]) j++;
+16        count += j - (mid + 1);
+17    }
+18
+19    // Merge step
+20    int[] temp = new int[right - left + 1];
+21    int i = left, k = 0;
+22    j = mid + 1;
+23    while (i <= mid && j <= right) {
+24        if (nums[i] <= nums[j]) temp[k++] = nums[i++];
+25        else temp[k++] = nums[j++];
+26    }
+27    while (i <= mid) temp[k++] = nums[i++];
+28    while (j <= right) temp[k++] = nums[j++];
+29    System.arraycopy(temp, 0, nums, left, temp.length);
+30
+31    return count;
+32}
+33
+34}
+35
