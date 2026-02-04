b58a27d7c8f549f3dbb386771b88de73e6fa6f5c
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2026-02-04 10:23:05 +0530

    Create 1299-ReplaceElementswithGreatestElementonRightSide

---
+++ 1299-ReplaceElementswithGreatestElementonRightSide/version/java/1299-ReplaceElementswithGreatestElementonRightSide_20260204_102301.java
@@ -0,0 +1,14 @@
+// Last updated: 2/4/2026, 10:23:01 AM
+1class Solution {
+2    public int[] replaceElements(int[] arr) {
+3        int n = arr.length;
+4        int maxi = -1;
+5
+6        for (int i = n - 1; i >= 0; i--) {
+7            int curr = arr[i];   // store original value
+8            arr[i] = maxi;       // replace with right-side max
+9            maxi = Math.max(maxi, curr); // update max
+10        }
+11        return arr;
+12    }
+13}
