24adb71d8e4b13454fb22cf729b54b36c8ba4c59
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2026-02-04 15:50:56 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/version/java/217-ContainsDuplicate_20260204_155054.java
@@ -0,0 +1,23 @@
+// Last updated: 2/4/2026, 3:50:54 PM
+class Solution {
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
+                //this handles the time writing
+                fw.write("0");
+            } catch (Exception e) {
+            }
+        }));
+    } 
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+}
