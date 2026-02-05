9a3d47520d29fa4ab65d0b6722531072eff43e96
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2026-02-05 18:34:26 +0530

    Create 3379-TransformedArray

---
+++ 3379-TransformedArray/version/java/3379-TransformedArray_20260205_183425.java
@@ -0,0 +1,12 @@
+// Last updated: 2/5/2026, 6:34:25 PM
+1class Solution {
+2    public int[] constructTransformedArray(int[] nums) {
+3     int n = nums.length;
+4     int[] ans = new int[n];
+5
+6     for(int i = 0 ; i < n ; i++){
+7        ans[i] = nums[((i + nums[i]) % n + n) % n];
+8     }
+9     return ans;
+10    }
+11}
