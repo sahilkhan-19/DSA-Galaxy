e5cbdd7f0ad2cde7c094c7935c632e6312153843
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2026-04-06 14:25:15 +0530

    Create 1755-DefuseTheBomb

---
+++ 1755-DefuseTheBomb/1755-DefuseTheBomb.java
@@ -0,0 +1,24 @@
+// Last updated: 4/6/2026, 2:25:13 PM
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int[] ans = new int[n];
+
+        if(k==0) return ans;
+
+        for(int i = 0 ; i < n ; i++){
+            int sum = 0;
+            if(k >0){
+                for(int j = 1; j<= k; j++){
+                    sum += code[(i + j) % n];
+                }
+            }else{
+                for(int j = 1 ; j <= -k ; j++){
+                    sum += code[(i - j + n) % n];
+                }
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+}
