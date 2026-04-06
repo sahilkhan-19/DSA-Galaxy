c02099914bbcef0e234cc3903a5e66bdf8cf2c22
Sahil Khan <145417279+sahilkhan-19@users.noreply.github.com>
2026-04-06 14:25:18 +0530

    Create 1444-NumberOfStepsToReduceANumberToZero

---
+++ 1444-NumberOfStepsToReduceANumberToZero/1444-NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,11 @@
+// Last updated: 4/6/2026, 2:25:15 PM
+class Solution {
+    public int numberOfSteps(int num) {
+        int count = 0;
+        while(num > 0){
+            num = ((num & 1)==0) ? num>>1 : num-1;
+            count++;
+        }
+        return count;
+    }
+}
